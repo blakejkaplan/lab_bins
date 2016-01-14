c12081c269c19536c6e7602f38a45480c7d1f6fc
blakejkaplan <blakejkaplan@gmail.com>
2016-01-14 17:46:04 -0500

    Refactored Bins for Recitation 1

--- README.md
+++ README.md
@@ -1,7 +1,48 @@
-# lab_bins
-First example code for refactoring
+bcw22, ert18, bjk20
 
-Making a change!!!
+Readability
 
-Even more changes :)
+What pieces of code help versus obscure your understanding of the algorithm?
+It obscures my understanding when almost all of the code is in the main method.
+It helps my understanding to have variable names which are descriptive of the data they stand for (for example, total and size when it comes to the files)
+
+What comments might be helpful within the code?
+It is helpful to explain what each method does, what the parameters in each method represent, and what the method is returning.
+
+Are there places where the code could be more concise and also more clear?
+Yes. The main method has four sections where the code is almost identical to one another and those areas could easily be clarified into one method.
+
+Testability
+
+How would you test this code for bugs?
+If I had pre-written unit tests I would just run those unit tests. If not, I would either use a debugger or print statements on small data sizes so that I could check the validity of the algorithm by hand.
+
+Give a specific example of a "test case" as the code is currently written.
+We ran the original (before refactoring) version of the code on the example.txt file. The output was then compared to the output of the refactored code.
+
+What additional functions may be helpful?
+A function which prints out the contents of the priority queue after the files are added to the disk
+A function which writes files to the disk
+
+Give a specific example of a "test case" for your new function.
+For the function which prints out the contents, run the program with the new function and before it was edited. Run a text diff on the two outputs to see if they’re any different.
+
+Extensibility
+
+What Code Smells can you find?
+The lines of code which printed the outputs were almost identical between the first and second runs
+The main method was very long and contained almost all of the logic for the entire program.
+
+What suggestions does this code make about how someone would extend it in the future to compare the performance of a wider variety of fitting algorithms?
+As it stands, the code doesn’t seem super extensible. Since almost everything is written in one method, it seems almost necessary to copy/paste similar code multiple times in that method in order to incorporate new fitting algorithms.
+However, this code can be made more extensible by refactoring and encapsulating different parts of the program into different methods. This is what our group did in the code we submitted.
+
+What dependencies are there between different parts of the code?
+The priority queue and list of data are both required by almost every part of the program.
+
+How easy to find are those dependencies?
+It is sort of clear, but given the size of the main method it requires a fair bit of reading.
+
+Can you clarify or remove those dependencies?
+These dependencies can be clarified by defining them in a setup method and then passing those variables into submethods.
 
--- src/Bins.java
+++ src/Bins.java
@@ -25,68 +25,97 @@ public class Bins {
         }
         return results;
     }
-
+    
     /**
-     * The main program.
+     * Populates the priority queue
+     * @param data
+     * The list of file sizes
+     * @param pq
+     * The priority queue used to process the files
+     * @param total
+     * Total size of all the files
+     * @param type
+     * Dictates whether or not the files are sorted by size from high to low
+     * @return
+     * The total size of all the files
      */
-    public static void main (String args[]) {
-        Bins b = new Bins();
+    private static int addToDisk(List<Integer> data, PriorityQueue<Disk> pq, Integer total, String type){
+    	int diskId = 1;
+        
+        for (Integer size : data) {
+            Disk d = pq.peek();
+            if (d.freeSpace() < size) {
+            	Disk d2 = new Disk(diskId);
+                diskId++;
+                d2.add(size);
+                pq.add(d2);
+            } else if ((type.equals("first") && d.freeSpace() > size) || 
+            		(type.equals("second"))) {
+            	pq.poll();
+                d.add(size);
+                pq.add(d);
+            }
+            total += size;
+        }
+        return total;
+    }
+    
+    /**
+     * Prints out the results of the algorithm
+     * @param type
+     * Dictates whether or not the input data is sorted
+     * @param total
+     * The total size of all the files
+     * @param pq
+     * The populated priority queue
+     */
+	private static void printInfo(String type, int total, PriorityQueue<Disk> pq) {
+
+		if (type.equals("unsorted")) {
+			System.out.println("total size = " + total / 1000000.0 + "GB");
+		}
+
+		System.out.println();
+		
+		if (type.equals("unsorted")) {
+			System.out.println("worst-fit method");
+		}
+		
+		else if (type.equals("decreasing")) {
+			System.out.println("worst-fit decreasing method");
+		}
+		System.out.println("number of pq used: " + pq.size());
+		while (!pq.isEmpty()) {
+			System.out.println(pq.poll());
+		}
+		System.out.println();
+
+	}
+    
+	/**
+	 * The initial setup and helper function calls
+	 */
+    private static void start(){
+    	Bins b = new Bins();
         Scanner input = new Scanner(Bins.class.getClassLoader().getResourceAsStream(DATA_FILE));
         List<Integer> data = b.readData(input);
 
         PriorityQueue<Disk> pq = new PriorityQueue<Disk>();
         pq.add(new Disk(0));
+        
+    	int total1 = addToDisk(data, pq, 0, "first");
+    	printInfo("unsorted", total1, pq);
+    	
+    	Collections.sort(data, Collections.reverseOrder());
+    	pq.add(new Disk(0));
+    	int total2 = addToDisk(data, pq, 0, "second");
+    	printInfo("decreasing", total2, pq);
+    }
 
-        int diskId = 1;
-        int total = 0;
-        for (Integer size : data) {
-            Disk d = pq.peek();
-            if (d.freeSpace() > size) {
-                pq.poll();
-                d.add(size);
-                pq.add(d);
-            } else {
-                Disk d2 = new Disk(diskId);
-                diskId++;
-                d2.add(size);
-                pq.add(d2);
-            }
-            total += size;
-        }
-
-        System.out.println("total size = " + total / 1000000.0 + "GB");
-        System.out.println();
-        System.out.println("worst-fit method");
-        System.out.println("number of pq used: " + pq.size());
-        while (!pq.isEmpty()) {
-            System.out.println(pq.poll());
-        }
-        System.out.println();
-
-        Collections.sort(data, Collections.reverseOrder());
-        pq.add(new Disk(0));
-
-        diskId = 1;
-        for (Integer size : data) {
-            Disk d = pq.peek();
-            if (d.freeSpace() >= size) {
-                pq.poll();
-                d.add(size);
-                pq.add(d);
-            } else {
-                Disk d2 = new Disk(diskId);
-                diskId++;
-                d2.add(size);
-                pq.add(d2);
-            }
-        }
-
-        System.out.println();
-        System.out.println("worst-fit decreasing method");
-        System.out.println("number of pq used: " + pq.size());
-        while (!pq.isEmpty()) {
-            System.out.println(pq.poll());
-        }
-        System.out.println();
+    /**
+     * The main program.
+     */
+    public static void main (String args[]) {
+        start();
     }
 }
